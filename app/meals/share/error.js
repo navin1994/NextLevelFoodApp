8999c3b8e1f2ee4672622bb6c48d1107b53eb48b
navin1994 <navin.yele@gmail.com>
2024-08-18 19:16:18 +0530

    Final project code

---
+++ app/meals/share/error.js
@@ -0,0 +1,8 @@
+'use client';
+
+export default function Error() {
+    return (<main className="error">
+        <h1>An Error Occurred!</h1>
+        <p>Error occurred while sharing meal data.</p>
+    </main>);
+}
